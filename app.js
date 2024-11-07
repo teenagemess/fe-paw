093039af9b3d41c8e1bce2a4a343e628a5da2c29
Raynanda Aqiyas Pramardhika <93027449+teenagemess@users.noreply.github.com>
2024-11-07 08:05:01 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
+});
